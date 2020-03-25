a2f4476982b721a520ee70ef15564dfe27c3c34f
kevinm <kvnmattes@gmail.com>
2020-03-25 13:05:03 -0400

    Added perimeter+sweep simulation, embedded Icons, blank map, drawing functions , rad levels and location functions.

--- GUI Bitmap Desktop/Radiation Map.Designer.cs
+++ GUI Bitmap Desktop/Radiation Map.Designer.cs
@@ -28,34 +28,30 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Radiashown));
             this.textBox1 = new System.Windows.Forms.TextBox();
             this.startButton = new System.Windows.Forms.Button();
             this.setupButton = new System.Windows.Forms.Button();
             this.emergencyButton = new System.Windows.Forms.Button();
             this.homeCheckBox = new System.Windows.Forms.CheckBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            this.panel1.SuspendLayout();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
             this.SuspendLayout();
-            // 
-            // pictureBox1
-            // 
-            this.pictureBox1.Location = new System.Drawing.Point(18, 18);
-            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(1284, 815);
-            this.pictureBox1.TabIndex = 0;
-            this.pictureBox1.TabStop = false;
-            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
             // 
             // textBox1
             // 
             this.textBox1.AcceptsReturn = true;
             this.textBox1.AllowDrop = true;
-            this.textBox1.Location = new System.Drawing.Point(1354, 807);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
-            this.textBox1.MaximumSize = new System.Drawing.Size(3000, 2000);
+            this.textBox1.Location = new System.Drawing.Point(1049, 298);
+            this.textBox1.MaximumSize = new System.Drawing.Size(2001, 2000);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(271, 26);
+            this.textBox1.Size = new System.Drawing.Size(182, 20);
             this.textBox1.TabIndex = 1;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
@@ -67,10 +63,9 @@
             this.startButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.startButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.startButton.ForeColor = System.Drawing.SystemColors.ControlText;
-            this.startButton.Location = new System.Drawing.Point(1353, 53);
-            this.startButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.startButton.Location = new System.Drawing.Point(1049, 12);
             this.startButton.Name = "startButton";
-            this.startButton.Size = new System.Drawing.Size(262, 78);
+            this.startButton.Size = new System.Drawing.Size(175, 51);
             this.startButton.TabIndex = 4;
             this.startButton.Text = "Start Sweep";
             this.startButton.UseVisualStyleBackColor = false;
@@ -83,10 +78,9 @@
             this.setupButton.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
             this.setupButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.setupButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.setupButton.Location = new System.Drawing.Point(1354, 150);
-            this.setupButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.setupButton.Location = new System.Drawing.Point(1050, 75);
             this.setupButton.Name = "setupButton";
-            this.setupButton.Size = new System.Drawing.Size(262, 78);
+            this.setupButton.Size = new System.Drawing.Size(175, 51);
             this.setupButton.TabIndex = 5;
             this.setupButton.Text = "Setup";
             this.setupButton.UseVisualStyleBackColor = false;
@@ -99,10 +93,9 @@
             this.emergencyButton.FlatAppearance.BorderColor = System.Drawing.Color.DimGray;
             this.emergencyButton.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
             this.emergencyButton.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.emergencyButton.Location = new System.Drawing.Point(1353, 150);
-            this.emergencyButton.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
+            this.emergencyButton.Location = new System.Drawing.Point(1049, 75);
             this.emergencyButton.Name = "emergencyButton";
-            this.emergencyButton.Size = new System.Drawing.Size(262, 78);
+            this.emergencyButton.Size = new System.Drawing.Size(175, 51);
             this.emergencyButton.TabIndex = 6;
             this.emergencyButton.Text = "Emergency Stop";
             this.emergencyButton.UseVisualStyleBackColor = false;
@@ -112,44 +105,89 @@
             // homeCheckBox
             // 
             this.homeCheckBox.AutoSize = true;
-            this.homeCheckBox.Location = new System.Drawing.Point(1404, 845);
+            this.homeCheckBox.Location = new System.Drawing.Point(1082, 322);
+            this.homeCheckBox.Margin = new System.Windows.Forms.Padding(2);
             this.homeCheckBox.Name = "homeCheckBox";
-            this.homeCheckBox.Size = new System.Drawing.Size(163, 24);
+            this.homeCheckBox.Size = new System.Drawing.Size(110, 17);
             this.homeCheckBox.TabIndex = 7;
             this.homeCheckBox.Text = "Robots Are Home";
             this.homeCheckBox.UseVisualStyleBackColor = true;
             this.homeCheckBox.Click += new System.EventHandler(this.home_Click);
             // 
+            // panel1
+            // 
+            this.panel1.AutoScroll = true;
+            this.panel1.AutoSize = true;
+            this.panel1.Controls.Add(this.pictureBox1);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1003, 1003);
+            this.panel1.TabIndex = 8;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(0, 0);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(1000, 1000);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox2.InitialImage")));
+            this.pictureBox2.Location = new System.Drawing.Point(1032, 962);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(50, 50);
+            this.pictureBox2.TabIndex = 9;
+            this.pictureBox2.TabStop = false;
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.InitialImage = ((System.Drawing.Image)(resources.GetObject("pictureBox3.InitialImage")));
+            this.pictureBox3.Location = new System.Drawing.Point(1102, 962);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(48, 48);
+            this.pictureBox3.TabIndex = 10;
+            this.pictureBox3.TabStop = false;
+            // 
             // Radiashown
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.AutoScroll = true;
             this.BackColor = System.Drawing.SystemColors.AppWorkspace;
-            this.ClientSize = new System.Drawing.Size(1676, 892);
+            this.ClientSize = new System.Drawing.Size(1269, 1061);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.panel1);
             this.Controls.Add(this.textBox1);
             this.Controls.Add(this.homeCheckBox);
             this.Controls.Add(this.emergencyButton);
             this.Controls.Add(this.setupButton);
             this.Controls.Add(this.startButton);
-            this.Controls.Add(this.pictureBox1);
-            this.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.Name = "Radiashown";
             this.Text = "Radiashown";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.PictureBox pictureBox1;
         private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Button startButton;
         private System.Windows.Forms.Button setupButton;
         private System.Windows.Forms.Button emergencyButton;
         private System.Windows.Forms.CheckBox homeCheckBox;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
     }
 }
 
--- GUI Bitmap Desktop/Radiation Map.cs
+++ GUI Bitmap Desktop/Radiation Map.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,34 +11,194 @@ using System.Windows.Forms;
 using IronPython.Hosting;
 using System.IO;
 using System.Runtime.InteropServices;
+using System.Drawing.Imaging;
+using System.Web;
+using System.Drawing.Design;
 
 namespace GUI_Bitmap_Desktop
 {
     public partial class Radiashown : Form
     {
-        Bitmap image1, image2;
+        Bitmap myBitmap;
         public Radiashown()
         {
             InitializeComponent();
         }
-
-        private void pictureBox1_Click(object sender, EventArgs e)
+        private Bitmap DrawFilledRectangle(int x, int y)
         {
-            //bitmap
-            //image1 = new Bitmap(@"./itb137.bmp", true);
+            Bitmap myBitmap = new Bitmap(x, y);
+            using (Graphics graph = Graphics.FromImage(myBitmap))
+            {
+                Rectangle ImageSize = new Rectangle(0, 0, x, y);
+                graph.FillRectangle(Brushes.White, ImageSize);
+            }
 
-            //load bmp in picturebox1
-            pictureBox1.Image = image1;
+            return myBitmap;
         }
 
         private void Form1_Load(object sender, EventArgs e)
         {
             //AllocConsole();
+            //panel1.Controls.Add(pictureBox1);
+        }
+
+        private void startButton_Click(object sender, EventArgs e)
+        {
+            startButton.BackColor = Color.FromName("Green");
+            startButton.Text = "Sweeping";
+            startButton.Enabled = false;
+            emergencyButton.Visible = true;
+            setupButton.Visible = false;
+
+            startScan();
+        }
+
+        private void emergencyButton_Click(object sender, EventArgs e)
+        {
+            startButton.BackColor = Color.FromName("FireBrick");
+            startButton.Text = "Returning Home";
+            startButton.Enabled = true;
+
+            emergencyButton.Visible = false;
+        }
+
+        //This is a placeholder
+        private void home_Click(object sender, EventArgs e)
+        {
+            startButton.BackColor = Color.FromName("Buttonshadow");
+            startButton.Text = "Start Sweep";
+            startButton.Enabled = true;
+
+            emergencyButton.Visible = false;
+            setupButton.Visible = true;
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+            startScan();
+        }
+        private void startScan()
+        {
+            //Graphics gr = CreateGraphics();
+            myBitmap = DrawFilledRectangle(1000, 1000);
+            pictureBox1.Image = myBitmap;
+            //Bitmap robotIcon = new Bitmap("C:/Users/Marilyn/Desktop/Engineering 5/4TB6/Icon/moon-rover50.png");
+            //pictureBox2.Image = robotIcon;
+            pictureBox2.Image = pictureBox2.InitialImage;
+            //Bitmap radIcon = new Bitmap("C:/Users/Marilyn/Desktop/Engineering 5/4TB6/Icon/radioactive-48.png");
+            //pictureBox3.Image = radIcon;
+            pictureBox3.Image = pictureBox3.InitialImage;
+            //myBitmap.SetResolution(500, 500);
+            //pictureBox1.SizeMode = PictureBoxSizeMode.AutoSize;
+
+            simulationPerimeter();
+            
+        }
+
+        private void DrawRadInfo(int xPos, int yPos, int radLevel)
+        {
+            var radColour = Brushes.White;
+            using (Graphics graph = Graphics.FromImage(myBitmap))
+            {
+                Rectangle ImageSize = new Rectangle(xPos, yPos, 25, 25); 
+                if (radLevel < 2)
+                {
+                    radColour = Brushes.Yellow;
+                }
+                if (radLevel > 1 & radLevel < 5)
+                {
+                    radColour = Brushes.Orange;
+                }
+                if (radLevel > 4 & radLevel < 10)
+                {
+                    radColour = Brushes.Red;
+                }
+                if (radLevel > 9)
+                {
+                    radColour = Brushes.DarkRed;
+                }
+                graph.FillRectangle(radColour, ImageSize);
+            }
+
+        }
+
+        private void simulationSweep()
+        {
+
+            for (int Xcount = 50; Xcount < 950; Xcount = Xcount + 25)
+            {
+                for (int Ycount = 50; Ycount < 950; Ycount = Ycount + 25)
+                {
+                    //robotLocation(Xcount, Ycount);
+                    if (Xcount % 25 == 0 & Ycount % 25 == 0)
+                    {
+                        DrawRadInfo(Xcount, Ycount, RandomNumber(0, 11));
+                    }
+                    pictureBox1.Refresh();
+
+                }
+            }
+        }
+        public int RandomNumber(int min, int max)
+        {
+            Random random = new Random();
+            return random.Next(min, max);
+        }
+
+        private void simulationPerimeter()
+        {
+            radLocation(500, 500);
+            for (int Ycount = 50; Ycount < 950; Ycount++)
+            {
+                myBitmap.SetPixel(50, Ycount, Color.Black);
+                robotLocation(50, Ycount);
+                pictureBox1.Refresh();
+            }
+
+            for (int Xcount = 50; Xcount < 950; Xcount++)
+            {
+                myBitmap.SetPixel(Xcount, 950, Color.Black);
+                robotLocation(Xcount, 950);
+                pictureBox1.Refresh();
+            }
+
+            for (int Ycount = 950; Ycount > 50; Ycount--)
+            {
+                myBitmap.SetPixel(950, Ycount, Color.Black);
+                robotLocation(950, Ycount);
+                pictureBox1.Refresh();
+            }
+
+            for (int Xcount = 950; Xcount > 50; Xcount--)
+            {
+                myBitmap.SetPixel(Xcount, 50, Color.Black);
+                robotLocation(Xcount, 50);
+                pictureBox1.Refresh();
+            }
+            simulationSweep();
+        }
+
+        private void radLocation(int x, int y)
+        {
+            pictureBox3.Location = new Point(x, y);
+            pictureBox3.Refresh();
+        }
+        private void robotLocation(int x, int y)
+        {
+            pictureBox2.Location = new Point(x, y);
+            pictureBox2.Refresh();
+        }
+        private void setupButton_Click(object sender, EventArgs e)
+        {
+            //this.Hide();
+            radSetup fsetup = new radSetup();
+            fsetup.ShowDialog();
+            //this.Close();
         }
 
         private void textBox1_TextChanged(object sender, EventArgs e)
         {
-            textBox1.Text = "test";
+            /*textBox1.Text = "test";
             // 1) Create engine
             var engine = Python.CreateEngine();
 
@@ -64,53 +225,84 @@ namespace GUI_Bitmap_Desktop
 
 
             // Display the pixel format in Label1.
-            textBox1.Text = str(results.ToArray());
+            textBox1.Text = str(results.ToArray());*/
         }
-          
-
-        private void startButton_Click(object sender, EventArgs e)
-        {
-            startButton.BackColor = Color.FromName("Green");
-            startButton.Text = "Sweeping";
-            startButton.Enabled = false;
-
-            emergencyButton.Visible = true;
-            setupButton.Visible = false;
-        }
-
-        private void emergencyButton_Click(object sender, EventArgs e)
-        {
-            startButton.BackColor = Color.FromName("FireBrick");
-            startButton.Text = "Returning Home";
-            startButton.Enabled = true;
-
-            emergencyButton.Visible = false;
-        }
-
-        //This is a placeholder
-        private void home_Click(object sender, EventArgs e)
-        {
-            startButton.BackColor = Color.FromName("Buttonshadow");
-            startButton.Text = "Start Sweep";
-            startButton.Enabled = true;
-
-            emergencyButton.Visible = false;
-            setupButton.Visible = true;
-        }
-
-        private void setupButton_Click(object sender, EventArgs e)
-        {
-            //this.Hide();
-            radSetup fsetup = new radSetup();
-            fsetup.ShowDialog();
-            //this.Close();
-        }
-
-
-        /*[DllImport("kernel32.dll", SetLastError = true)]
-        [return: MarshalAs(UnmanagedType.Bool)]
-        static extern bool AllocConsole();
-        */
-
     }
 }
+/*
+        private void MainScreenThread()
+        {
+            ReadData();//reading data from socket.
+            initial = bufferToJpeg();//first intial full screen image.
+            pictureBox1.Paint += pictureBox1_Paint;//activating the paint event.
+            while (true)
+            {
+                int pos = ReadData();
+                x = BlockX();//where to draw :X
+                y = BlockY();//where to draw :Y
+                Bitmap block = bufferToJpeg();//constantly reciving blocks.
+                Draw(block, new Point(x, y));//applying the changes-drawing the block on the big initial image.using native memcpy.
+
+                this.Invoke(new Action(() =>
+                {
+                    pictureBox1.Refresh();//updaing the picturebox for seeing results.
+                                          // this.Text = ((pos / 1000).ToString() + "KB");
+                }));
+            }
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            lock (initial)
+            {
+                e.Graphics.DrawImage(initial, pictureBox1.ClientRectangle); //draws at picturebox's bounds
+            }
+        }
+
+        private unsafe void Draw(Bitmap bmp2, Point point)
+        {
+            lock (initial)
+            {
+                BitmapData bmData = initial.LockBits(new Rectangle(0, 0, initial.Width, initial.Height), System.Drawing.Imaging.ImageLockMode.WriteOnly, initial.PixelFormat);
+                BitmapData bmData2 = bmp2.LockBits(new Rectangle(0, 0, bmp2.Width, bmp2.Height), System.Drawing.Imaging.ImageLockMode.ReadOnly, bmp2.PixelFormat);
+                IntPtr scan0 = bmData.Scan0;
+                IntPtr scan02 = bmData2.Scan0;
+                int stride = bmData.Stride;
+                int stride2 = bmData2.Stride;
+                int Width = bmp2.Width;
+                int Height = bmp2.Height;
+                int X = point.X;
+                int Y = point.Y;
+
+                scan0 = IntPtr.Add(scan0, stride * Y + X * 3);//setting the pointer to the requested line
+                for (int y = 0; y < Height; y++)
+                {
+                    memcpy(scan0, scan02, (UIntPtr)(Width * 3));//copy one line
+
+                    scan02 = IntPtr.Add(scan02, stride2);//advance pointers
+                    scan0 = IntPtr.Add(scan0, stride);//advance pointers//
+                }
+
+
+                initial.UnlockBits(bmData);
+                bmp2.UnlockBits(bmData2);
+            }
+        }*/
+
+/*if (Xcount > scaleX ^ Ycount > scaleY)
+{                       
+//myBitmap = new Bitmap(myBitmap, new Size(myBitmap.Width / 4, myBitmap.Height / 4));
+//myBitmap.SetResolution(2500, 2500);
+pictureBox1.Image = myBitmap;
+using (Graphics graph = Graphics.FromImage(myBitmap))
+{
+    DrawFilledRectangle(500, 500);
+    var x = (500 - myBitmap.Width) / 2;  
+    var y = (500 - myBitmap.Height) / 2;  
+    graph.DrawImageUnscaled(myBitmap, x, y, myBitmap.Width, myBitmap.Height);
+}
+//Xcount = Xcount / 4;
+//Ycount = Ycount / 4;
+System.Threading.Thread.Sleep(5000);
+}*/
+
